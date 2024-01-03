c1ceef2b1a6fe84130f061306a9d323ac7b4a032
piyushcodes07 <piyushcodes07@gmail.com>
2024-01-03 15:32:45 +0530

    implmented express.Router()

--- app.js
+++ app.js
@@ -1,92 +1,87 @@
-const fs = require('fs') 
-const express = require('express')
-const { log } = require('console')
-const app = express()
-const PORT = 3000
+const fs = require('fs');
+const express = require('express');
+const { log } = require('console');
+const app = express();
+const PORT = 3000;
+const morgan = require('morgan');
+const { memoryUsage } = require('process');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.use(express.json());
+app.use(morgan('dev'));
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)) 
-app.use(express.json())
+const getAllTours = (req, res) => {
+  res.json({
+    status: 'success',
+    data: {
+      tours: tours,
+    },
+  });
+};
 
-const getAllTours = (req,res)=>{
+const getTour = (req, res) => {
+  let id = req.params.id * 1; //converts the string to number
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'error', data: 'no tour found' });
+  }
 
-    res.json({
-        status:"success",
-        data:{
-            tours:tours
-        }
-    })
-    
-}
+  console.log(req.params);
+  //req.params has all the dynamic route variables
+  //optional params can be defined like this '/api/v1/tours/:id/:optional?'
 
-const getTour = (req,res)=>{
+  const tour = tours.find((Element) => Element.id === id);
 
-    let id = req.params.id * 1  //converts the string to number
-     if(id>tours.length){
-        res.status(404).json({status:"error",data:"no tour found"})
-     }
-    
-    console.log(req.params);
-    //req.params has all the dynamic route variables
-    //optional params can be defined like this '/api/v1/tours/:id/:optional?'
+  res.json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 
-    const tour  = tours.find(Element=>Element.id===id)
+const createTour = (req, res) => {
+  let newTour = req.body;
 
-    res.json({
-        status:"success",
-        data:{
-            tour
-        }
-    })
-    
-}
+  res.json({
+    status: 'success',
+    data: {
+      req: req.body,
+    },
+  });
+};
 
-const createTour = (req,res)=>{
+const updateTour = (req, res) => {
+  res.status(201).json({
+    status: 'success',
+    data: 'your updated data here',
+  });
+};
 
-    let newTour  = req.body
-
-
-    res.json({
-        status:"success",
-        data:{
-            req:req.body
-        }
-    })
-    
-}
-
-const updateTour = (req,res)=>{
-
-    res.status(201).json({
-        status:"success",
-        data:"your updated data here"
-    })
-
-}
-
-const deleteTour = (req,res)=>{
-
-    res.status(204).json({
-        status:"success",
-        data:null
-    })
-
-}
-
-// app.get('/api/v1/tours',getAllTours)
-
-// app.get('/api/v1/tours/:id',getTour)
-
-// app.post('/api/v1/tours',createTour)
-
-// app.patch('/api/v1/tours/:id',updateTour)
-
-// app.delete('/api/v1/tours/:id',deleteTour)
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour)
-
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
 
 
-app.listen(PORT,()=>{
-    console.log(`server is up on ${PORT}`);
-})
+const toursRouter = express.Router() // creating a router instead of ( const app = express() )
+// const userRouter  = express.Router()
+app.use('/api/v1/tours',toursRouter); // routers are used as middleware for the followinig route
+
+
+toursRouter
+.route('/')
+.get(getAllTours)
+.post(createTour);
+
+toursRouter
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.listen(PORT, () => {
+  console.log(`server is up on ${PORT}`);
+});
