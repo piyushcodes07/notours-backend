17c7d91e437bb1f32682045a73c3896b6c5e37ca
piyushcodes07 <piyushcodes07@gmail.com>
2024-01-07 17:19:58 +0530

    CRUD with mongoose completed!

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,34 +1,40 @@
 
 const Tour = require('.././models/tourModel')
 
-exports.getAllTours = (req, res) => {
-    res.json({
-      status: 'success',
-      data: {
-        // tours: tours,
-      },
-    });
+exports.getAllTours = async(req, res) => {
+    
+  try {
+    const allTours = await Tour.find()
+    res.status(200).json({
+      message:"sucess",
+      data:allTours
+    })  
+  } catch (error) {
+    res.status(404).json({
+      status:"error",
+      message:error
+    })
+  }
+  
   };
 
-exports.getTour = (req, res) => {
-    // let id = req.params.id * 1; //converts the string to number
-    // if (id > tours.length) {
-    //   return res.status(404).json({ status: 'error', data: 'no tour found' });
-    // }
-  
-    // console.log(req.params);
-    //req.params has all the dynamic route variables
-    //optional params can be defined like this '/api/v1/tours/:id/:optional?'
-  
-    // const tour = tours.find((Element) => Element.id === id);
-  
-    res.json({
-      status: 'success',
-      data: {
-        // tour,
-      },
-    });
-  };
+exports.getTour = async (req, res) => {
+    try {
+      let id = req.params.id ;
+
+      const tour = await Tour.findById(id)
+      res.status(200).json({
+        status:"sucess",
+        data:tour
+      })
+    
+    } catch (error) {
+      res.status(404).json({
+        status:"fail",
+        data:error
+      })
+    }
+  }
 
 
   
@@ -48,17 +54,37 @@ exports.getTour = (req, res) => {
     }
   };
   
-  exports.updateTour = (req, res) => {
-    res.status(201).json({
-      status: 'success',
-      data: 'your updated data here',
-    });
+  exports.updateTour =  async (req, res) => {
+    try {
+      const tour = await Tour.findByIdAndUpdate(req.params.id,req.body,{
+        new:true, // returns the newly created object
+        runValidators:true // runs the validator defined in Schema
+      })  
+      res.status(201).json({
+        status: 'success',
+        data: tour,
+      });
+    } catch (error) {
+      res.status(200).json({
+        status: 'error',
+        message: error,
+      });
+    }
+    
   };
   
-  exports.deleteTour = (req, res) => {
-    res.status(204).json({
-      status: 'success',
-      data: null,
-    });
+  exports.deleteTour = async (req, res) => {
+    try {
+      await Tour.findByIdAndDelete(req.params.id)
+      res.status(202).json({
+        status:"success",
+        message:""
+      })
+    } catch (error) {
+      res.json({
+        status:"fail",
+        message:error
+      })
+    }
   };
   
